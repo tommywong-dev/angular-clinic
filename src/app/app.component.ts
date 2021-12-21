7ebfcd585a89e0f99a50cfe049e74367b072b8f8
Tommy Wong <solsedu@sols247.org>
2021-12-21 16:02:59 +0800

    override theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'clinic';
+
+  constructor(public router: Router) {}
 }
